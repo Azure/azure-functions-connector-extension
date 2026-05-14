8b5716ac14521d54228980e0cb96a511028c397f
Victoria Hall <victoriahall@microsoft.com>
2026-05-14 15:24:42 -0500

    Update sample to only process single message

--- samples/python/README.md
+++ samples/python/README.md
@@ -65,15 +65,14 @@ curl -X POST "http://localhost:7071/runtime/webhooks/connector?functionName=OnNe
 
 ```python
 @app.function_name(name="OnNewEmail")
-@app.connector_trigger(arg_name="emails")
-def on_new_email(emails: List[office365.ClientReceiveMessage]) -> None:
+@app.connector_trigger(arg_name="email")
+def on_new_email(email: office365.ClientReceiveMessage) -> None:
     """
     Receives Office 365 email trigger callbacks from Connector Namespace managed connectors
     and saves to blob storage.
     """
-    logging.info("OnNewEmail trigger received. Payload: %s", emails)
+    logging.info("OnNewEmail trigger received. Payload: %s", email)
 
-    for email in emails:
-        logging.info(f"Subject: {email.subject}")
-        logging.info(f"From: {email.from_}")
+    logging.info(f"Subject: {email.subject}")
+    logging.info(f"From: {email.from_}")
 ```
--- samples/python/function_app.py
+++ samples/python/function_app.py
@@ -8,20 +8,18 @@ and saves the payload to blob storage.
 import azure.functions as func
 import azurefunctions.extensions.connectors.office365 as office365
 import logging
-from typing import List
 
 app = func.FunctionApp()
 
 
 @app.function_name(name="OnNewEmail")
-@app.connector_trigger(arg_name="emails")
-def on_new_email(emails: List[office365.ClientReceiveMessage]) -> None:
+@app.connector_trigger(arg_name="email")
+def on_new_email(email: office365.ClientReceiveMessage) -> None:
     """
     Receives Office 365 email trigger callbacks from Connector Namespace managed connectors
     and saves to blob storage.
     """
-    logging.info("OnNewEmail trigger received. Payload: %s", emails)
+    logging.info("OnNewEmail trigger received. Payload: %s", email)
 
-    for email in emails:
-        logging.info(f"Subject: {email.subject}")
-        logging.info(f"From: {email.from_}")
+    logging.info(f"Subject: {email.subject}")
+    logging.info(f"From: {email.from_}")
